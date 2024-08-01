fc636391062f5f2225d1eac2910ccec87422423b
Webitya <webitya@gmail.com>
2024-08-01 08:02:32 +0530

    Form Validation Basic

---
+++ index.js
@@ -0,0 +1,12 @@
+let style=document.querySelector("style");
+let bodyy=document.querySelector("#bodyy")
+let script=document.querySelector(".script");
+let html1=document.querySelector(".html");
+let css=document.querySelector(".css");
+let javascript=document.querySelector(".javascript");
+javascript.innerHTML=script.innerHTML;
+css.innerHTML=style.innerHTML;
+html1.innerHTML=bodyy.innerHTML;
+
+
+
